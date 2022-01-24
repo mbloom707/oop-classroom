8c8e9bbc5134e28a313bf3765774fb0f5a37ab8c
mbloom707 <mlbloom707@gmail.com>
2022-01-24 09:03:38 -0800

    Added incomplete marker.js assignemnt

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,24 @@
 class Marker {
-  
+  constructor(size, color, remainingInk) {
+    this.size = size;
+    this.color = color;
+    this.remainingInk = remainingInk;
+  }
+
+  write(string) {
+    let strLength = string.split(' ');
+
+    if(this.remainingInk >= strLength) {
+      this.remainingInk -= strLength;
+      return string;
+    } else {
+      // Add check to see how much is remaining
+      // let remainingStr -= strLength // Would this work?
+      this.remainingInk = 0;
+      // Find method to find to write out what can still be wrote
+      // Add ability to still write spaces
+    }
+  }
 }
 
 module.exports = Marker
