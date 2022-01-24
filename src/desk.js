ab020cf788c0af8b7a05c647134aac65ae901cbd
mbloom707 <mlbloom707@gmail.com>
2022-01-24 09:02:41 -0800

    Added desk.js assignment

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,19 @@
 class Desk {
-  
+  constructor(length, width, height, isWhiteboard) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.isWhiteboard = isWhiteboard;
+    this.content = '';
+  }
+
+    write(string) {
+      this.content = this.content + string;
+    }
+
+    wipe() {
+      if (this.isWhiteboard) this.content = '';
+    }
 }
 
 module.exports = Desk
