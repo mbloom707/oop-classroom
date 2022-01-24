bb98f791a77881529e1ee6dfcec88e3c1993d91b
mbloom707 <mlbloom707@gmail.com>
2022-01-24 09:03:59 -0800

    Added incomplete student.js assignment

--- src/student.js
+++ src/student.js
@@ -1,5 +1,21 @@
 class Student {
-  
+  constructor(name, skillLevel, assignments) {
+    this.name = name;
+    this.skillLevel = skillLevel;
+    this.assignments = [];
+  }
+
+  study() {
+    if(this.skillLevel < 100) {
+      this.skillLevel += 1;
+      return this;
+    }
+  }
+
+  doHomework(assignment) {
+    // Create 'let' of skillLevel on homework and compare to student skillLevel
+    // Create check to see if 'assignment' is not present / If not, attempt to complete homework in array
+  }
 }
 
 module.exports = Student
